af331da373de6584e7d27b6bfbaa50ae387759b0
Eric <eric.ebersohl.1@gmail.com>
2019-05-07 15:21:15 -0500

    feat: new weight test

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def play_game(color: Color, weight: float) -> Tuple[float, int, float]:
     state = get_default_state(color)
     while not check_victory(state):
         if state.to_move == Color.BLACK:
-            action = default_mcts(state, 10, weight)
+            action = default_mcts(state, 100, weight)
         else:
-            action = default_mcts(state, 10, 2.0)
+            action = default_mcts(state, 100, 2.0)
         depth += 1
         branch.append(len(get_actions(state)))
         state = get_next_state(state, action)
@@ -42,7 +42,7 @@ def test_weight_factor(sims: int):
 
     # play 20 games
     for game in range(sims):
-        print(f'{game}, ', end='')
+        print(f'({game}/{sims})')
         if game%2 == 0:
             starting_player == Color.BLACK
         else:
@@ -56,20 +56,36 @@ def test_weight_factor(sims: int):
     line = [sum(points), weight, mean(depth), mean(branch)]
 
     # output results to csv
-    with open ('./output/weight.csv', mode='a') as weight_file:
+    with open ('./build/weight.csv', mode='a') as weight_file:
         weight_writer = csv.writer(weight_file, delimiter=',', quoting=csv.QUOTE_MINIMAL, quotechar='"')
         weight_writer.writerow(line)
         print("writing:",line)
 
-n = 3
+# courtesy of https://gist.github.com/thatalextaylor/7408395
+def pretty_time_delta(seconds):
+    seconds = int(seconds)
+    days, seconds = divmod(seconds, 86400)
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    if days > 0:
+        return '%dd%dh%dm%ds' % (days, hours, minutes, seconds)
+    elif hours > 0:
+        return '%dh%dm%ds' % (hours, minutes, seconds)
+    elif minutes > 0:
+        return '%dm%ds' % (minutes, seconds)
+    else:
+        return '%ds' % (seconds,)
+
+n = 20
+m = 200
+
 start = datetime.now()
-test_weight_factor(n)
+
+for j in range(m):
+    print(f'Beginning run {j+1} of {m}')
+    test_weight_factor(n)
+
 end = datetime.now()
+diff = end - start
 
-diff: timedelta = end - start
-days = diff.days
-hours = diff.seconds // 3600
-minutes = (diff.seconds // 3600) % 60
-seconds = ((diff.seconds // 3600) % 60) % 60
-
-print(f"Ran {n} simulations in {days} days, {hours} hours, {minutes} minutes, {seconds} seconds.")
+print(f'\nRan {n} simulations in {pretty_time_delta(diff.total_seconds())}')
